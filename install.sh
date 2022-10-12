86943064ba2de17eb649b375b6f87d71da46383f
ES-Yukun <79987160+ES-Yukun@users.noreply.github.com>
2022-10-13 04:12:06 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,7 +13,6 @@ set ruler
 set history=1000
 set encoding=utf8
 syntax enable
-
 " PLUGIN SETTINGS
 call plug#begin('~/.config/nvim/plugged')
 Plug 'vim-airline/vim-airline'
@@ -25,6 +24,11 @@ Plug 'ryanoasis/vim-devicons'
 Plug 'tomasiser/vim-code-dark'
 call plug#end()
 
+function! s:check_back_space() abort
+  let col = col('.') - 1
+  return !col || getline('.')[col - 1]  =~ '\s'
+endfunction
+
 " NERDTree SETTINGS
 nmap <C-f> :NERDTreeToggle<CR>
 let g:airline#extensions#tabline#enabled = 1
@@ -33,10 +37,18 @@ nmap <C-n> <Plug>AirlineSelectNextTab
 inoremap { {}<LEFT>
 inoremap ( ()<LEFT>
 inoremap [ []<LEFT>
+inoremap " ""<LEFT>
+inoremap ' ''<LEFT>
+inoremap ` ``<LEFT>
 inoremap <C-s> <ESC>:w<CR>i
 nnoremap q :q<CR>
 nnoremap w :w<CR>
 nnoremap <C-s> :w<CR>
+nnoremap <C-RIGHT> <C-w>v<Plug>AirlineSelectPrevTab
+inoremap <silent><expr> <Tab> pumvisible() ? "\<C-n>" : <SID>check_back_space() ? "\<Tab>" :coc#refresh()
+inoremap <expr> <ENTER> pumvisible() ? coc#_select_confirm() : "\<ENTER>"
+inoremap <expr> <Tab> pumvisible() ? "\<C-n>" : "\<Tab>"
+inoremap <expr> <S-Tab> pumvisible() ? "\<C-p>" : "\<S-Tab>"
 
 " Airline SETTINGS
 let g:airline_powerline_fonts = 1
@@ -52,12 +64,10 @@ let g:airline#extensions#hunks#non_zero_only = 1
 let g:airline#extensions#default#layout = [[ 'a', 'b', 'c' ], ['z']]
 let g:airline_section_c = '%t %M'
 let g:airline_section_z = get(g:, 'airline_linecolumn_prefix', '').'%3l:%-2v'
-
 colorscheme codedark
 
 " Esc SETTINGS
 inoremap jk <Esc>
-
 " /// Enable Netrw (default file browser)
 " filetype plugin on
 " /// Netrw SETTINGS
@@ -68,7 +78,6 @@ inoremap jk <Esc>
 " let g:netrw_sizestyle = "H"
 " let g:netrw_timefmt = "%Y/%m/%d(%a) %H:%M:%S"
 " let g:netrw_preview = 1
-
 "/// SPLIT BORDER SETTINGS
 hi VertSplit cterm=none
 EOF
