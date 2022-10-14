d7b009e6603cbf546a38c4146923576f3c5d896d
ES-Yukun <79987160+ES-Yukun@users.noreply.github.com>
2022-10-14 15:45:29 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 mkdir -p ~/.config/nvim/
